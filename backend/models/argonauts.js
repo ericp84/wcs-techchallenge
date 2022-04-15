c63133fe8ea79b96bc3b7f03a13e4eefdb1c88be
Eric Paoli <ericpaoli65@gmail.com>
2022-04-15 12:21:00 +0200

    untrack backend

--- backend/models/argonauts.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require ('mongoose');
-
-let argonautSchema = mongoose.Schema({
-    name: String,
-})
-
-const argonautModel = mongoose.model('argonauts', argonautSchema);
-
-module.exports = argonautModel;
